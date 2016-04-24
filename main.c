afe838123887f24f3899c6b4c6494e9346950829
Vladislav Savyonok <savionak@gmail.com>
2016-04-24 10:08:01 +0300

    Pids are saved correctly
    + synchronized handlers setting and start of signals exchanging

--- main.c
+++ main.c
@@ -45,7 +45,7 @@ const unsigned char GROUP_TYPE[] =
     { 0, 1, 0, 2, 0, 0, 0, 1, 1 };
 /*    0  1  2  3  4  5  6  7  8  */
 
-/* Receivers numbers:
+/* Whome to send signal:
  *
  * 0 = none
  * positive = pid
@@ -53,9 +53,26 @@ const unsigned char GROUP_TYPE[] =
  *
  * f/ex: "-x" means, that signal is sent to the group, process with pid = x is member of
  */
-const char RECEIVER[] =
+const char NEXT_RECEIVER[] =
     { 0, -6, 1, 2, -3, 0, 4, 4, 4 };
 /*    0,  1, 2, 3,  4, 5, 6, 7, 8  */
+
+const int SIGNALS[] =
+{
+    0,          /* 0 */
+    SIGUSR1,    /* 1 */
+    SIGUSR2,    /* 2 */
+    0,          /* 3 */
+    SIGUSR1,    /* 4 */
+    0,          /* 5 */
+    SIGUSR1,    /* 6 */
+    SIGUSR2,    /* 7 */
+    SIGUSR1     /* 8 */
+};
+
+const char SIGNALS_COUNT[] =
+    { 0, 0, 1, 1, 3, 0, 1, 1, 1 };
+/*    0, 1, 2, 3, 4, 5, 6, 7, 8  */
 
 
 char *exec_name = NULL;
@@ -65,28 +82,20 @@ int proc_id = 0;
 void forker(int curr_number, int childs_count);
 
 char *pids_list_path = "/tmp/pids.log";
-int pids_list = NULL;
 
 void wait_for_children();
-
+pid_t *pids_list = NULL;
 
 int main(int argc, char *argv[])
 {
     exec_name = basename(argv[0]);
 
-    if ( (pids_list = open(pids_list_path, O_RDWR | O_CREAT | O_TRUNC) ) == -1) {
-        print_error(exec_name, "Can't open temp file", 0);
-        exit(1);
-    }
-
-    mmap(pids_list, (PROC_COUNT+1)*sizeof(pid_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    pids_list = (pid_t*)mmap(pids_list, (PROC_COUNT+1)*sizeof(pid_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
     int i = 0;
-    pid_t p = 0;
-
-    lseek(pids_list, 0, SEEK_SET);                      // initialize pids list with zeros [1]
-    for (i = 0; i < PROC_COUNT; ++i) {
-        write(pids_list, &p, sizeof(pid_t));
+                                                        // initialize pids list with zeros [1]
+    for (i = 0; i <= PROC_COUNT; ++i) {
+        pids_list[i] = 0;
     }
 
 
@@ -101,10 +110,7 @@ int main(int argc, char *argv[])
 
     on_exit(&wait_for_children, NULL);
 
-
-    p = getpid();                                       // save pid to list
-    lseek(pids_list, proc_id*sizeof(pid_t), SEEK_SET);
-    write(pids_list, &p, sizeof(pid_t));
+    pids_list[proc_id] = getpid();          // save pid to list
 
 #ifdef DEBUG
     printf("%d\tpid: %d\tppid: %d\tpgid: %d\n", proc_id, getpid(), getppid(), getpgid(0));
@@ -114,71 +120,69 @@ int main(int argc, char *argv[])
 
     if (proc_id == STARTING_PROC_ID) {                  // starter waits for all pids available [3]
         do{
-            p = 1;
-            lseek(pids_list, sizeof(pid_t), SEEK_SET);
+            for (i = 1; (i <= PROC_COUNT) && (pids_list[i] != 0); ++i) {
 
-            for (i = 0; (i < PROC_COUNT) && (p != 0); ++i) {
-                read(pids_list, &p, sizeof(pid_t));
-
-                if (p == -1) {
+                if (pids_list[i] == -1) {
                     print_error(exec_name, "Error: not all processes forked or initialized.\nHalting.", 0);
                     exit(1);
                 }
             }
 //            printf("%d\t", i);
-        } while (p == 0);
+        } while (i < PROC_COUNT);
 
 #ifdef DEBUG
         printf("All pids are set!\n");
-//        int times = 0;
+
+        for (i = 0; (i < PROC_COUNT); ++i) {
+            printf("%d - %d\n", i, pids_list[i]);
+        }
+
+        int times = 0;
 #endif
 
-/*
+        pids_list[0] = 1;           // all pids are set
+
         // <------------------ set starter handler here (may be the same as usual handler)
 
-        p = 0;
-        lseek(pids_list, STARTING_PROC_ID*sizeof(pid_t), SEEK_SET);   // handler is set
-        write(pids_list, &p, sizeof(pid_t));
-
+        pids_list[STARTING_PROC_ID] = 0;                // handler is set
 
         do{                                             // starter waits for all handlers to be set [6]
 #ifdef DEBUG
         ++times;
 #endif
-            p = 0;
-            lseek(pids_list, sizeof(pid_t), SEEK_SET);
-            for (i = 0; (i < PROC_COUNT) && (p == 0); ++i) {
-                read(pids_list, &p, sizeof(pid_t));
+            for (i = 1; (i < PROC_COUNT) && (pids_list[i] == 0); ++i) {
             }
-            printf(">%d\n", i);
-        } while (p != 0);
+//            printf(">%d\n", i);
+        } while (pids_list[i] != 0);
 
-#ifdef DEBUG
-        printf("Ready to start signals-flow after %d rotations\n", times);
-#endif
+        #ifdef DEBUG
+                printf("Ready to start signals-flow after %d rotations\n", times);
 
+                for (i = 0; (i < PROC_COUNT); ++i) {
+                    printf("%d - %d\n", i, pids_list[i]);
+                }
+        #endif
         // <<=========   start signal ping-pong here [7]
-*/
+
         return 0;
     }
 
 // other processes
 
-    do {                                // wait for all pids to be written
-        lseek(pids_list, 0, SEEK_SET);
-        read(pids_list, &p, sizeof(pid_t));
-    } while (p == 0);
+    do {
+        // wait for all pids to be written
+    } while (pids_list[0] == 0);
 
     // <---------------------- set handler here
+
+    pids_list[proc_id] = 0;             // notify starting_proc, that handler is set
+
 /*
-    p = 0;
-    lseek(pids_list, (proc_id-1)*sizeof(pid_t), SEEK_SET);  // reset pid - notify, that handler is set [5]
-    write(pids_list, &p, sizeof(pid_t));
+    while (1) {
+        pause();  // start cycle
+    }
 */
 
-//    pause();
-
-    close (pids_list);
     return 0;
 }   /*  main   */
 
@@ -198,9 +202,7 @@ void wait_for_children() {
 void print_error(const char *s_name, const char *msg, const int proc_num) {
     fprintf(stderr, "%s: %s %d\n", s_name, msg, proc_num);
 
-    int err = -1;
-    lseek(pids_list, proc_num*sizeof(pid_t), SEEK_SET);
-    write(pids_list, &err, sizeof(pid_t));
+    pids_list[proc_num] = -1;
 
     exit(1);
 }   /*  print_error */
